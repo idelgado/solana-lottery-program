2777fd692f5a06df540bb9c6283d93dfb983e3de
Jack Rieck <jack@Jacks-MacBook-Air.local>
2022-02-07 18:24:53 -0600

    Init vault implementation

--- programs/no-loss-lottery/src/lib.rs
+++ programs/no-loss-lottery/src/lib.rs
@@ -1,14 +1,224 @@
 use anchor_lang::prelude::*;
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token,
+    token::{self},
+};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod no_loss_lottery {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        _vault_bump: u8,
+        _vault_mgr_bump: u8,
+        draw: i64,
+    ) -> ProgramResult {
+        // set vault manager config
+        let vault_mgr = &mut ctx.accounts.vault_manager;
+        vault_mgr.draw = draw;
+        vault_mgr.mint = ctx.accounts.mint.clone().key();
+        vault_mgr.vault = ctx.accounts.vault.clone().key();
+
+        Ok(())
+    }
+
+    pub fn deposit(
+        ctx: Context<Deposit>,
+        _vault_bump: u8,
+        _vault_mgr_bump: u8,
+        amount: u64,
+    ) -> ProgramResult {
+        // transfer tokens from user wallet to vault
+
+        let transfer_accounts = token::Transfer {
+            from: ctx.accounts.user_ata.clone().to_account_info(),
+            to: ctx.accounts.vault.clone().to_account_info(),
+            authority: ctx.accounts.user.clone().to_account_info(),
+        };
+
+        token::transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.clone().to_account_info(),
+                transfer_accounts,
+            ),
+            amount,
+        )
+    }
+
+    pub fn withdraw(
+        ctx: Context<Withdraw>,
+        _vault_bump: u8,
+        vault_mgr_bump: u8,
+        amount: u64,
+    ) -> ProgramResult {
+        // get current timestamp from Clock program
+        let now = Clock::get()?.unix_timestamp;
+
+        // if time remaining then error
+        if now < ctx.accounts.vault_manager.draw {
+            return Err(ErrorCode::TimeRemaining.into());
+        }
+
+        let transfer_accounts = token::Transfer {
+            from: ctx.accounts.vault.clone().to_account_info(),
+            to: ctx.accounts.user_ata.clone().to_account_info(),
+            authority: ctx.accounts.vault_manager.clone().to_account_info(),
+        };
+
+        // transfer tokens from vault to user wallet
+        token::transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.clone().to_account_info(),
+                transfer_accounts,
+                &[&[
+                    ctx.accounts.mint.key().as_ref(),
+                    ctx.accounts.vault.key().as_ref(),
+                    &[vault_mgr_bump],
+                ]],
+            ),
+            amount,
+        )
+    }
+
+    pub fn draw(ctx: Context<Draw>, _vault_bump: u8, _vault_mgr_bump: u8) -> ProgramResult {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(vault_bump: u8, vault_mgr_bump: u8)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub mint: Account<'info, token::Mint>,
+
+    #[account(init,
+        payer = user,
+        token::mint = mint,
+        token::authority = vault_manager,
+        seeds = [mint.key().as_ref()],
+        bump = vault_bump, has_one = mint)]
+    pub vault: Account<'info, token::TokenAccount>,
+
+    #[account(init,
+        payer = user,
+        seeds = [mint.key().as_ref(), vault.key().as_ref()],
+        bump = vault_mgr_bump)]
+    pub vault_manager: Account<'info, VaultManager>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, token::Token>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+#[instruction(vault_bump: u8, vault_mgr_bump: u8)]
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub mint: Account<'info, token::Mint>,
+
+    #[account(mut,
+        seeds = [mint.key().as_ref()],
+        bump = vault_bump, has_one = mint)]
+    pub vault: Account<'info, token::TokenAccount>,
+
+    #[account(mut,
+        has_one = vault,
+        has_one = mint,
+        seeds = [mint.key().as_ref(), vault.key().as_ref()],
+        bump = vault_mgr_bump)]
+    pub vault_manager: Account<'info, VaultManager>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(mut, has_one = mint)]
+    pub user_ata: Account<'info, token::TokenAccount>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, token::Token>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+#[instruction(vault_bump: u8, vault_mgr_bump: u8)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub mint: Account<'info, token::Mint>,
+
+    #[account(mut,
+        seeds = [mint.key().as_ref()],
+        bump = vault_bump, has_one = mint)]
+    pub vault: Account<'info, token::TokenAccount>,
+
+    #[account(mut,
+        has_one = vault,
+        has_one = mint,
+        seeds = [mint.key().as_ref(), vault.key().as_ref()],
+        bump = vault_mgr_bump)]
+    pub vault_manager: Account<'info, VaultManager>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(mut, has_one = mint)]
+    pub user_ata: Account<'info, token::TokenAccount>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, token::Token>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+#[instruction(vault_bump: u8, vault_mgr_bump: u8)]
+pub struct Draw<'info> {
+    #[account(mut)]
+    pub mint: Account<'info, token::Mint>,
+
+    #[account(mut,
+        seeds = [mint.key().as_ref()],
+        bump = vault_bump, has_one = mint)]
+    pub vault: Account<'info, token::TokenAccount>,
+
+    #[account(mut,
+        has_one = vault,
+        has_one = mint,
+        seeds = [mint.key().as_ref(), vault.key().as_ref()],
+        bump = vault_mgr_bump)]
+    pub vault_manager: Account<'info, VaultManager>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, token::Token>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct VaultManager {
+    pub mint: Pubkey,
+    pub vault: Pubkey,
+    //pub lock: i64, // in ms, when lock is triggered deposits and withdrawals are disabled until draw time
+    pub draw: i64, // in ms, lottery end time
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("TimeRemaining")]
+    TimeRemaining,
+}
+
+// n % m
+// https://stackoverflow.com/questions/31210357/is-there-a-modulus-not-remainder-function-operation
+fn n_mod_m <T: std::ops::Rem<Output = T> + std::ops::Add<Output = T> + Copy>
+  (n: T, m: T) -> T {
+    ((n % m) + m) % m
+}
+
--- tests/no-loss-lottery.ts
+++ tests/no-loss-lottery.ts
@@ -1,17 +1,155 @@
-import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import { NoLossLottery } from '../target/types/no_loss_lottery';
+import * as anchor from "@project-serum/anchor";
+import * as spl from "@solana/spl-token";
+import * as assert from "assert";
+import * as switchboard from "@switchboard-xyz/switchboard-api";
+import { Program } from "@project-serum/anchor";
+import { NoLossLottery } from "../target/types/no_loss_lottery";
 
-describe('no-loss-lottery', () => {
-
+describe("no-loss-lottery", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.NoLossLottery as Program<NoLossLottery>;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  it("Smoke", async () => {
+    const mintAuthority = await newAccountWithLamports(
+      program.provider.connection
+    );
+
+    // create mint for testing
+    const mint = await spl.Token.createMint(
+      program.provider.connection,
+      mintAuthority,
+      mintAuthority.publicKey,
+      null,
+      9,
+      spl.TOKEN_PROGRAM_ID
+    );
+    console.log("test mint created");
+
+    // get PDAs
+
+    const [vault, vaultBump] = await anchor.web3.PublicKey.findProgramAddress(
+      [mint.publicKey.toBuffer()],
+      program.programId
+    );
+
+    const [vaultMgr, vaultMgrBump] =
+      await anchor.web3.PublicKey.findProgramAddress(
+        [mint.publicKey.toBuffer(), vault.toBuffer()],
+        program.programId
+      );
+
+    // lottery draw timestamp (future)
+    const drawMs = 3 * 1000;
+    const now = new Date().getTime();
+    const draw = new anchor.BN(new Date(now + drawMs).getTime() / 1000);
+
+    // init vault
+    const initTxSig = await program.rpc.initialize(
+      vaultBump,
+      vaultMgrBump,
+      draw,
+      {
+        accounts: {
+          mint: mint.publicKey,
+          vault: vault,
+          vaultManager: vaultMgr,
+          user: program.provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          tokenProgram: spl.TOKEN_PROGRAM_ID,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        },
+      }
+    );
+    console.log("initTxSig:", initTxSig);
+
+    // get user ata
+    const userAta = await mint.getOrCreateAssociatedAccountInfo(
+      program.provider.wallet.publicKey
+    );
+
+    // mint tokens to user_ata
+    await mint.mintTo(userAta.address, mintAuthority.publicKey, [], 100);
+    console.log("minted 100 tokens to user_ata");
+
+    // deposit tokens into vault
+    const depositTxSig = await program.rpc.deposit(
+      vaultBump,
+      vaultMgrBump,
+      new anchor.BN(1),
+      {
+        accounts: {
+          mint: mint.publicKey,
+          vault: vault,
+          vaultManager: vaultMgr,
+          user: program.provider.wallet.publicKey,
+          userAta: userAta.address,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          tokenProgram: spl.TOKEN_PROGRAM_ID,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        },
+      }
+    );
+    console.log("depositSigTx:", depositTxSig);
+
+    // wait for draw to expire
+    await sleep(drawMs + 500);
+
+    // draw winner
+    const drawTxSig = await program.rpc.draw(vaultBump, vaultMgrBump, {
+      accounts: {
+        mint: mint.publicKey,
+        vault: vault,
+        vaultManager: vaultMgr,
+        user: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        tokenProgram: spl.TOKEN_PROGRAM_ID,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      },
+    });
+    console.log("drawTxSig:", drawTxSig);
+
+    // user withdraw tokens + any winnings
+    const withdrawTxSig = await program.rpc.withdraw(
+      vaultBump,
+      vaultMgrBump,
+      new anchor.BN(1),
+      {
+        accounts: {
+          mint: mint.publicKey,
+          vault: vault,
+          vaultManager: vaultMgr,
+          user: program.provider.wallet.publicKey,
+          userAta: userAta.address,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          tokenProgram: spl.TOKEN_PROGRAM_ID,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        },
+      }
+    );
+    console.log("withdrawTxSig:", withdrawTxSig);
   });
 });
+
+// create new Account and seed with lamports
+async function newAccountWithLamports(
+  connection: anchor.web3.Connection,
+  lamports: number = 100_000_000_000_000
+): Promise<anchor.web3.Account> {
+  // generate keypair
+  const account = new anchor.web3.Account();
+
+  // airdrop lamports
+  let txSig = await connection.requestAirdrop(account.publicKey, lamports);
+  await connection.confirmTransaction(txSig);
+  console.log("airdrop tx-sig: ", txSig);
+
+  return account;
+}
+
+// slep current thread in milliseconds
+async function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
