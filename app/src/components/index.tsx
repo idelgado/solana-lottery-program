5b5e6d6c60fdd950fbc273619e3d48b99c5dcf72
id <i@d.com>
2022-02-25 09:31:33 -0800

    Initial dapp scaffolding

---
+++ app/src/components/index.tsx
@@ -0,0 +1,2 @@
+export { Loader } from "./Loader";
+export { SelectAndConnectWalletButton } from "./SelectAndConnectWalletButton";
---
+++ app/src/views/HomeView/index.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import { FC } from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+import { SolanaLogo } from "components";
+import styles from "./index.module.css";
+
+export const HomeView: FC = ({}) => {
+  const { publicKey } = useWallet();
+
+  const onClick = () => {};
+
+  return (
+    <div className="container mx-auto max-w-6xl p-8 2xl:px-0">
+      <div className={styles.container}>
+        <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
+          <div className="flex-none">
+            <button className="btn btn-square btn-ghost">
+              <span className="text-4xl">🎰</span>
+            </button>
+          </div>
+          <div className="flex-1 px-2 mx-2">
+            <span className="text-lg font-bold">No Loss Lottery</span>
+          </div>
+          <div className="flex-none">
+            <WalletMultiButton className="btn btn-ghost" />
+          </div>
+        </div>
+
+        <div className="text-center pt-2">
+          <div className="hero min-h-16 py-4">
+            <div className="text-center hero-content">
+              <div className="max-w-lg">
+                <h1 className="mb-5 text-5xl font-bold">
+                  No Loss Lottery
+                </h1>
+                <p className="mb-5">
+                  Solana wallet adapter is connected and ready to use.
+                </p>
+                <p>
+                  {publicKey ? <>Your address: {publicKey.toBase58()}</> : null}
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
