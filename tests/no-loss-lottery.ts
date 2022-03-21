2a5fa1e3ea83300d63724312ab281a091c84890e
id <i@d.com>
2022-03-21 17:21:58 -0500

    Use node_module spl-token dep

--- tests/no-loss-lottery.ts
+++ tests/no-loss-lottery.ts
@@ -1,5 +1,6 @@
 import * as anchor from "@project-serum/anchor";
-import * as spl from "@solana/spl-token";
+// Avoid linking to switchboard spl-token dependency
+import * as spl from "../node_modules/@solana/spl-token";
 import * as assert from "assert";
 import * as tokenSwap from "@solana/spl-token-swap";
 import {
